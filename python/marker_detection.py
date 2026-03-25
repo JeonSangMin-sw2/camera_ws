d4c5a78128c9a0027342dae8ee1ce7a865a899dd
jundol119 <jundol22@naver.com>
2026-03-25 09:10:16 +0000

    bug fix:t5 to marker pose error

--- python/marker_detection.py
+++ python/marker_detection.py
@@ -797,7 +797,7 @@ class Marker_Transform:
         self.Stereo = Stereo
         # self.client = TCPClient("127.0.0.1", 5000)
         # Setup Transforms
-        T5_to_marker_data = [0.022, 0.0, 0.18, 180, 0.0, -90.0]
+        T5_to_marker_data = [0.022, 0.0, 0.121, 180, 0.0, -90.0]
         # T5_to_marker_data = [0,0,0,0,0,0]
         tool_to_cam = [0,0,0,0,0,0]
         # tool_to_cam = [0.009,-0.09,-0.085,159,0,180]
