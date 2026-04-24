f995029910dc4cce63f26a8f796947077844f6ec
sangmin.jeon <sangmin.jeon@rainbow-robotics.com>
2026-04-24 15:43:08 +0900

    deleted:    developer/test_links.py 	deleted:    developer/test_sdk.py 	deleted:    developer/test_sdk2.py

--- developer/test_links.py
+++
@@ -1,7 +0,0 @@
-import rby1_sdk as rby
-try:
-    robot = rby.create_robot("192.168.30.1:50051", "a")
-    robot.connect()
-    print("Joint names:", robot.model().robot_joint_names)
-except Exception as e:
-    print("Unable to connect:", e)
--- developer/test_sdk.py
+++
@@ -1,6 +0,0 @@
-import rby1_sdk as rby
-robot = rby.create_robot("192.168.30.1:50051", "a")
-robot.connect()
-model = robot.model()
-print("dir(model):", dir(model))
-# Try to find link info
