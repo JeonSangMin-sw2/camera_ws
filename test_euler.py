b91cef6ff12d63e217cc207ab9b879f03ad3eb4c
sangmin.jeon <sangmin.jeon@rainbow-robotics.com>
2026-05-07 19:08:00 +0900

    deleted:    scratch/check_dataset.py 	deleted:    test_euler.py 	deleted:    test_yaw.py

--- test_euler.py
+++
@@ -1,8 +0,0 @@
-from core.calibration_core import make_transform
-import numpy as np
-
-T1 = make_transform([0, 0, 0, 90, 0, 180])
-T2 = make_transform([0, 0, 0, -90, 0, 180])
-
-print("T1 (Roll=90, Yaw=180):\n", np.round(T1[:3, :3], 2))
-print("T2 (Roll=-90, Yaw=180):\n", np.round(T2[:3, :3], 2))
